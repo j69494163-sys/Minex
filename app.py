7e4d006feed7584c37ebca36f5609bbabecf1a8f
j69494163-sys <j69494163@gmail.com>
2026-04-30 20:14:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, render_template_string, redirect
+import os
+
 
 app = Flask(__name__)
 
@@ -52,5 +54,8 @@ def view(user):
 
     return redirect(f"https://www.google.com/maps?q={loc['lat']},{loc['lng']}")
 
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
